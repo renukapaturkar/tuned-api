ab8eddd7cdc764bbcf3648fd9896af616a877c96
Renuka Paturkar <67357703+renukapaturkar@users.noreply.github.com>
2021-07-28 16:22:46 +0530

    feature: added custom routes with user managment.

--- models/customplaylist.model.js
+++ models/customplaylist.model.js
@@ -4,9 +4,10 @@ const { Videos } = require("./video.model.js");
 const { Schema } = mongoose;
 
 const CustomPlaylistSchema = new Schema({
-  name: { type: String, required: true },
-  Playlists: [{ type: mongoose.Schema.Types.ObjectId, ref: "Videos" }],
-});
+  user: {type:Schema.Types.ObjectId, ref: 'User'},
+  name:{type:String},
+  videos: [{_id:{type: Schema.Types.ObjectId, ref: "Videos"}}],
+}, {timestamps:true});
 
 const CustomPlaylist = mongoose.model("CustomPlaylist", CustomPlaylistSchema);
 
--- routes/customplaylist.router.js
+++ routes/customplaylist.router.js
@@ -7,12 +7,13 @@ const {CustomPlaylist} = require('../models/customplaylist.model.js')
 router.route('/')
 .get(async(req, res)=> {
     try {
-        const playlists = await CustomPlaylist.find({});
+        const user = req.user;
+        const playlists = await CustomPlaylist.find({user:user._id})
     if(!playlists){
         res.status(404).json({success: false, message: "Cannot find the playlist Data."});
 
     }else {
-        res.json({success: true, playlistData : playlists})
+        res.status(200).json({success: true, playlistData : playlists})
     }
 
     }catch(error){
@@ -22,23 +23,54 @@ router.route('/')
 
 .post(async(req, res)=> {
     try {
-        const newPlaylistData = req.body;
-        const newPlaylist = new CustomPlaylist(newPlaylistData);
+        const playlist = req.body;
+        console.log(playlist)
+        const user = req.user;
+        const newPlaylist = await new CustomPlaylist({user: user._id, name:playlist.name, videos:{_id:playlist.videos._id}});
         const savedPlaylist = await newPlaylist.save();
         console.log(newPlaylist, "new playlist")
-        console.log(savedPlaylist)
-        const data = await CustomPlaylist.findById(savedPlaylist._id).populate("Playlists")
-        res.json(data);
+        console.log(savedPlaylist, "saved playlist at 31")
+        const data = await CustomPlaylist.findOne({user: user._id}).populate("vidoes._id")
+        console.log(data, "line 33")
+        res.status(200).json({success: true, playlistData: data});
     }catch(error){
         res.status(500).json({success: false, message: "Internal Server Error", errMessage: error.message})
     }
 })
 
 
-// router.route(':/playlistId')
-// .post(async(req, res)=> {
-//     const {playlistId} = req.params;
-//     const {}
-// })
+router.route('/:playlistId')
+.post(async(req, res)=> {
+    try {
+        const {playlistId} = req.params;
+        const playlist = req.body;
+        console.log("videodata", playlist, playlistId)
+        const user = req.user;
+        await CustomPlaylist.updateOne({"user": user._id, "_id":playlistId}, {$addToSet:{"videos":playlist}} )
+        const data = await CustomPlaylist.findOne({user:user._id}).populate("videos")
+        console.log("line 50 - data",data);
+        res.status(200).json({success: true, playlistData: data})
+
+    }catch(error){
+        res.status(500).json({success: false, message: "unable to store data", errMessage:error.message})
+    }
+})
+
+
+
+router.route("/:playlistId/:videoId")
+.delete(async(req,res)=> {
+    try {
+        const {playlistId, videoId} = req.params;
+        const user = req.user;
+        await CustomPlaylist.updateOne({"user":user._id, "_id":playlistId}, {$pull: {videos:{_id: videoId}}});
+        const data = await CustomPlaylist.findOne({user: user._id}).populate("videos");
+        res.status(200).json({success: true, playlistData: data})
+    }catch(error){
+        res.status(500).json({success: false, message: "Internal Server Error", errMessage: error.message})
+    }
+
+
+})
 
 module.exports = router;
